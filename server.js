0028e56ed4fe023964ccda15ee154e392eb9e59e
Chay Dahill <cfdahill@gmail.com>
2018-10-25 15:24:24 -0700

    in serv.js added index.html to res.sendFile

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
   app.use('/static', express.static(path.join(__dirname, './client/build')));
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './client/build'));
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
   });
 }
 
