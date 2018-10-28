bec6029f229130e4791126d90eed270c0ce80017
Chay Dahill <cfdahill@gmail.com>
2018-10-28 11:24:54 -0700

    fixed portfolio url

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
     <footer>
       <div className='footerOverlay'>
       <p>This site is not affiliated  with Blizzard Entertainment</p>
-      <p>Created by <a href="https://chay.dahill.com" target="blank">Chay Dahill</a></p>
+      <p>Created by <a href="https://chaydahill.com" target="blank">Chay Dahill</a></p>
       <p>2018</p>
       </div>
     </footer>
